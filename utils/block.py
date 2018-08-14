3acbe315d3e05912711e6e12f14e7648e2ece921
micey969 <micey_969@hotmail.com>
2018-08-14 09:45:10 -0400

    fix(indentation): convert spaces to tabs

--- utils/block.py
+++ utils/block.py
@@ -19,24 +19,24 @@ def blockTransform(block) -> dict:
 	"""
 	data = {
 		"number": block["number"],
-	    "hash": w3.toHex(block["hash"]),
-	    "parentHash": w3.toHex(block["parentHash"]),
-	    "mixHash": w3.toHex(block["mixHash"]),
-	    "nonce": w3.toHex(block["nonce"]),
-	    "sha3Uncles": w3.toHex(block["sha3Uncles"]),
-	    "logsBloom": w3.toHex(block["logsBloom"]),
-	    "transactionsRoot": w3.toHex(block["transactionsRoot"]),
-	    "stateRoot": w3.toHex(block["stateRoot"]),
-	    "receiptsRoot": w3.toHex(block["receiptsRoot"]),
-	    "miner": block["miner"],
-	    "difficulty": block["difficulty"],
-	    "totalDifficulty": block["totalDifficulty"],
-	    "extraData": w3.toHex(block["extraData"]),
-	    "size": block["size"],
-	    "gasLimit": block["gasLimit"],
-	    "gasUsed": block["gasUsed"],
-	    "timestamp": block["timestamp"],
-	    "uncles": block["uncles"]
+		"hash": w3.toHex(block["hash"]),
+		"parentHash": w3.toHex(block["parentHash"]),
+		"mixHash": w3.toHex(block["mixHash"]),
+		"nonce": w3.toHex(block["nonce"]),
+		"sha3Uncles": w3.toHex(block["sha3Uncles"]),
+		"logsBloom": w3.toHex(block["logsBloom"]),
+		"transactionsRoot": w3.toHex(block["transactionsRoot"]),
+		"stateRoot": w3.toHex(block["stateRoot"]),
+		"receiptsRoot": w3.toHex(block["receiptsRoot"]),
+		"miner": block["miner"],
+		"difficulty": block["difficulty"],
+		"totalDifficulty": block["totalDifficulty"],
+		"extraData": w3.toHex(block["extraData"]),
+		"size": block["size"],
+		"gasLimit": block["gasLimit"],
+		"gasUsed": block["gasUsed"],
+		"timestamp": block["timestamp"],
+		"uncles": block["uncles"]
 	}
 	txns = []
 	for txn in block["transactions"]:
